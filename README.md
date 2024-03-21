ba0ae84770e261bfd92e90cd2779528f99df5e29
Willmor Pena <willmor.pena@yahoo.com>
2024-03-21 18:48:05 -0400

    updated README

--- README.md
+++ README.md
@@ -1,61 +1,42 @@
 # DLVO-WORKFLOW: Molecular Dynamics of DLVO Ions
-Workflow runs MD simulations of DLVO ions using the HOOMD Blue MD engine. 
+
+DLVO-Workflow runs MD simulations of DLVO ions using the HOOMD Blue MD engine. 
 Workflow takes input from user and generates input files for various simulations
-having different parameters which results in different crystalization behavior
-key parameters include the list below
-To run: 
-        python3 -u run_parameter_sweep_dlvo.py [OPTIONS]
+having different parameters which results in different crystalization behavior.
+After simulations are over the predicted trajectories are analyzed to determine clustering.
+
+The workflow is orchestrated with the
+[Parsl parallel scripting library](https://parsl-project.org/) 
+
+Key parameters editable in the xml form include the list below.
 
 Parameter Options:
 
+	lattice_repeats: Times to replicate the system in each direction
+			
+	lattice_spacing: Lattice spacing in terms of positive particle diameter 
+			
+	fraction_positive: Fraction positive charge 
+		
+	radiusN: Radius of negative colloids 
+		
+	radiusP: Radius of positive colloids
 
-        lattice_repeats:
-                command line name: '-a'
-                nargs="*"
-                default=None
-                type=int
+    debye_length: Debye length 
+             
+    seed: Random seed 
+		
+    yaml_file: template yaml file
 
-        lattice_spacing:
-                command line name: '-LS'
-                nargs="*"
-                default=None
-                type=float
-	
-    	fraction_positive:
-		command line name:'-FP'
-		nargs="*" 
-		default=None
-		type=float
+	output_dir: parent location of simulation outputs
 
-	radiusN:
-		command line name: '-RN'
-    		nargs="*"
-		default=None
- 		type=float
-	
-	radiusP:
-    		command line name: '-RP'
-		nargs="*"
-		default=None
-		dest="radiusP"
-		type=float
+## Contents
 
-        debye_length:
-                command line: '-DL'
-                 nargs="*"
-                 default=None
-                 type=float
-	
-    	seed:
-		command line name: '-seed'
-		nargs="*"
-		default=None
-		type=int
-
-    	yaml_file: 
-		command line name: '-yaml_file'
-		default='input_general.yaml'
-
-	output_dir:
-		command line name: '-output_dir'
-		default='binary_dlvo' 
++ **`./thumb`:** contains thumbnail for workflow.
++ **`./parsl_utils`:** contains scripts necessay to set up local and remote environments.
++ **`./inputs`:** contains a general input file in yaml format which will be edited and copied to corresponding running directories.
++ **`./utils`:** contains input preparation code and core functionality code that runs MD simulations.
++ **`./requirements`:** contains yaml files specifying packages needed by this workflow.  These dependencies are installed automatically by `parsl_utils`.
++ **`parsl_wrapper.sh`:** bash script to run scripts in parsl_utils.
++ **`main.py`:** is the the workflow code submitting and managing jobs.
++ **`workflow.xml`:** is the file that takes user input in xml format for the workflow
--- utils/run_dlvo_spheres_metal.py
+++ utils/run_dlvo_spheres_metal.py
@@ -95,48 +95,15 @@ def screened_potential_shifted_force(r,rmin,rmax,radius_sum,steric_prefactor,ele
 
 if __name__=="__main__":
     import yaml
-    '''
-    parser=argparse.ArgumentParser()
-    #parser.add_argument("-i","--inputfile",default=None,help="Input file (gsd file, optional)",type=str)
-    parser.add_argument("--gpu",default=False,action="store_true")
-    parser.add_argument("--closedwalls",default=False,help="Turn on closed walls along x/y/z directions (default: False)",action="store_true")
-    parser.add_argument("--gravity",default=0.5,help="Turn on downward gravity constant force in z by setting to a positive number (default: 0)",type=float)
-    parser.add_argument("--chargewall",default=0,help="Turn on wall perp to z attracting -(>0) or +(<0) particles (default: False)",type=float)
-    parser.add_argument("-RN","--radiusN",default=250.0,help="Radius of colloids (default: %(default)s)",type=float)
-    parser.add_argument("-RP","--radiusP",default=250.0,help="Radius of colloids (default: %(default)s)",type=float)
-    parser.add_argument("-B","--brush_length",help="Brush length (default: %(default)s)",default=10,type=float)
-    parser.add_argument("-s","--brush_density",help="Brush density (default: %(default)s)",default=0.09,type=float)
-    parser.add_argument("-d","--debye_length",help="Debye length (default: %(default)s)",default=4,type=float)
-    parser.add_argument("--surface_potentialN",help="Surface potential N in mV (default: %(default)s)",default=-50,type=float)
-    parser.add_argument("--surface_potentialP",help="Surface potential P in mV (default: %(default)s)",default=50,type=float)
-    parser.add_argument("--dielectric_constant",help="Dielectric constant (default: %(default)s)",default=80,type=float)
-    parser.add_argument("--gamma",help="Drag coefficient (default: %(default)s)",default=0.01,type=float)
-    parser.add_argument("--massP",help="Mass of a positive particle",default=1.0,type=float)
-    parser.add_argument("--massN",help="Mass of a negative particle",default=None,type=float)
-    parser.add_argument("--seed",help="Random seed (default: %(default)s)",default=1,type=int)
-    parser.add_argument("--fraction_positive",help="Fraction positive charge (default: %(default)s)",default=0.1,type=float)
-    parser.add_argument("-a","--lattice_spacing",help="Lattice spacing in terms of positive particle diameter (default: %(default)s)",default=1.5,type=float)
-    parser.add_argument("--lattice_repeats",default=5,type=int,help="times to repliacte the system in each direction (default: %(default)s)") #no of times we want to replicate in one direction
-    parser.add_argument("--lattice_type",default="bcc",help="Lattice type (bcc, sc, fcc) (default: %(default)s)")
-    parser.add_argument("--orbit_factor",default=1.3,type=float,help="Factor beyond sum of radii and brush at which to start N particles (default: %(default)s)")
-    parser.add_argument("--dt",help="Simulation time step (default: %(default)s)",type=float,default=0.1)
-    parser.add_argument("--seed_file",help="xyz file specifying some seed coordinates",default=None)
-    parser.add_argument("--scale_by",default="max_diameter",help="Scale xyz by this (default: %(default)s), options: max_diameter, max_radius")
-    #parser.add_argument("-o","--outputprefix",help="Output prefix (required)",type=str,required=True)
-    #parser.add_argument("-n","--nsteps",help="Number of steps to run",type=int,required=True)
-    #parser.add_argument("-T","--temperature",help="Temperature at which to run, or list of tuples specifying annealing schedule",default="1.0")
-    #parser.add_argument("-m","--mode",help="Integrator/dynamics scheme. Allowed: Minimize, Langevin, NVT (default: %(default)s)",default="Langevin")
-    args = parser.parse_args()
-    #locals().update(vars(args))
-    '''
 
-    print("Remote: Loading the yaml input file")
-    # running process must be in run directory
+    print("Remote:........ Loading the yaml input file........")
+
+    # running process must have cd'ed  into run directory
     with open ('inputs_new.yaml') as f:
             parameters = yaml.load(f,Loader=yaml.FullLoader)
     locals().update(parameters)
 
-    print("Remote: ======= ALL PARAMETERS =======")
+    print("Remote: ======= parameters read from yaml file  =======")
     print(parameters)
 
     # DECIDE WHETHER RUNNING ON GPU OR ON CPU
