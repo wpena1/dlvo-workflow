22853cb957dff0781f04eb60cc39980fc8048e54
Willmor Pena <willmor.pena@yahoo.com>
2024-03-26 22:54:37 -0400

    updated main.py

--- main.py
+++ main.py
@@ -27,27 +27,27 @@ if __name__=="__main__":
 
     print(F"MAIN.py:...starting workflow setup...\n Resource list: {resource_labels}")
 
-    lattice_repeats = [ int(a) for a in str(form_inputs['model_inputs']['lattice_repeats']).split('_')]
-    lattice_spacing = [float(ls) for ls in str(form_inputs['model_inputs']['lattice_spacing']).split('_')]
-    fraction_positive = [float(fp) for fp in str(form_inputs['model_inputs']['fraction_positive']).split()]
-    radiusN = [float(rn) for rn in str(form_inputs['model_inputs']['radiusN']).split()]
-    radiusP = [float(rp) for rp in str(form_inputs['model_inputs']['radiusP']).split()]
-    debye_length = [float(dl) for dl in str(form_inputs['model_inputs']['debye_length']).split()]
-    seed = [int(sd) for sd in str(form_inputs['model_inputs']['seed']).split()]
-    nsteps = [int(steps) for steps in str(form_inputs['model_inputs']['nsteps']).split()]
+    lattice_repeats = [ int(a) for a in str(form_inputs['parameters']['lattice_repeats']).split('_')]
+    lattice_spacing = [float(ls) for ls in str(form_inputs['parameters']['lattice_spacing']).split('_')]
+    fraction_positive = [float(fp) for fp in str(form_inputs['parameters']['fraction_positive']).split()]
+    radiusN = [float(rn) for rn in str(form_inputs['parameters']['radiusN']).split()]
+    radiusP = [float(rp) for rp in str(form_inputs['parameters']['radiusP']).split()]
+    debye_length = [float(dl) for dl in str(form_inputs['parameters']['debye_length']).split()]
+    seed = [int(sd) for sd in str(form_inputs['parameters']['seed']).split()]
+    nsteps = [int(steps) for steps in str(form_inputs['parameters']['nsteps']).split()]
 
     input_file = 'inputs/inputs.yaml'
-    output_directory = str(form_inputs['model_inputs']['output_dir'])
+    output_directory = str(form_inputs['parameters']['output_dir'])
 
     arg_dict = {'lattice_repeats': lattice_repeats, 'lattice_spacing': lattice_spacing, 'fraction_positive':fraction_positive, 'radiusN':radiusN, 'radiusP': radiusP, \
                 'debye_length': debye_length, 'seed': seed, 'yaml_file': input_file, 'output_dir': output_directory, 'nsteps':nsteps}
 
     run_dir = os.getcwd()
-    source_dir = os.path.join(run_dir, output_directory)
-    remote_dir = config.executors[0].working_dir + F"/{output_directory}"
+    output_dir = os.path.join(run_dir, output_directory)
+    remote_dir = os.path.join(config.executors[0].working_dir , output_directory)
 
     # print(arg_dict)
-    print('MAIN.py: output at: ', source_dir)
+    print('MAIN.py: output at: ', output_dir)
     print('MAIN.py: current directory: ', run_dir)
     print('MAIN.py: remote directory: ', remote_dir)
 
@@ -56,25 +56,25 @@ if __name__=="__main__":
     result_list = []
     full_paths = []
     
-    print("MAIN:.......Going in main loop to set up......")
-    print(f"MAIN:......number of input files {len(input_yaml_files)}")
+    print("MAIN.py:.......Going in main loop to set up......")
+    print(f"MAIN.py:......number of input files {len(input_yaml_files)}")
 
     for i,run_file in enumerate(input_yaml_files):
         print(f"=============== run: {i} ===============")
         run_file_dir = os.path.dirname(run_file)
         output_dir_remote = run_file_dir
-        output_dir_local = source_dir
-        output_dir = PWFile(url='file://usercontainer/' + output_dir_local, local_path=remote_dir + output_dir_remote)
-        print(F"MAIN:....output_dir: {output_dir}")
-        full_paths.append(output_dir)
+        output_dir_local = output_dir
+        output_dir_PW = PWFile(url='file://usercontainer/' + output_dir_local, local_path=remote_dir + output_dir_remote)
+        print(F"MAIN.py:....PW output_dir: {output_dir_PW}")
+        full_paths.append(output_dir_PW)
         # inputs = [output, Path('run_simulation.bash'),Path('run_dlvo_spheres_metal.py') ]
         inputs = [output_dir, nsteps[0]]
-        shutil.copy('utils/run_dlvo_spheres_metal.py', output_dir, follow_symlinks=True)
-        shutil.copy('utils/run_simulation.bash', output_dir, follow_symlinks=True)
-        shutil.copy('utils/clustering_analysis.py', output_dir, follow_symlinks=True)
-        shutil.copy('utils/render_frame.py', output_dir, follow_symlinks=True)
+        shutil.copy('utils/run_dlvo_spheres_metal.py', output_dir_PW, follow_symlinks=True)
+        shutil.copy('utils/run_simulation.bash', output_dir_PW, follow_symlinks=True)
+        shutil.copy('utils/clustering_analysis.py', output_dir_PW, follow_symlinks=True)
+        shutil.copy('utils/render_frame.py', output_dir_PW, follow_symlinks=True)
 
-        r = run_dlvo(inputs=inputs, outputs=[output_dir], stdout=remote_dir+output_dir_remote+"/run.out", stderr=remote_dir+output_dir_remote+"/run.err")
+        r = run_dlvo(inputs=inputs, outputs=[output_dir_PW], stdout=remote_dir+output_dir_remote+"/run.out", stderr=remote_dir+output_dir_remote+"/run.err")
         result_list.append(r)
 
     [r.result() for r in result_list]
@@ -94,10 +94,10 @@ if __name__=="__main__":
 
         rows.append(F"{in_a},{in_ls},{in_fp},{in_rn},{in_rp},{in_dl},{in_seed},{img_cluster}")
 
-    with open(F'{source_dir}/out.csv','w') as csvfile:
+    with open(F'{output_dir}/out.csv','w') as csvfile:
         csvfile.write("\n".join(rows))
-    with open(F'{source_dir}/out.html', 'w') as html:
-        csvpath=F'{source_dir}/out.csv'
+    with open(F'{output_dir}/out.html', 'w') as html:
+        csvpath=F'{output_dir}/out.csv'
         html.write("""\
         <html style="overflow-y:hidden;background:white">\
         <a style="font-family:sans-serif;z-index:1000;position:absolute;top:15px;right:0px;margin-right:20px;font-style:italic;font-size:10px"\
